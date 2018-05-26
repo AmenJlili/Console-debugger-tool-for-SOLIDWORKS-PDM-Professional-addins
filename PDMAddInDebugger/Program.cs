c2a9fb77e0b39b6975431d277a7daf469b9b4eeb
= <amen@cadsharp.com>
2018-05-25 22:08:00 -0700

    First committ

--- PDMAddInDebugger/Program.cs
+++ PDMAddInDebugger/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,52 @@ namespace PDMAddInDebugger
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+
+            var ConsoleDefaultColor = Console.ForegroundColor ;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(string.Format("CADSharp LLC - {0} {1}", System.Windows.Forms.Application.ProductName, System.Windows.Forms.Application.ProductVersion));
+            Console.ForegroundColor = ConsoleDefaultColor;
+            Console.WriteLine(string.Format("Debugging started at: {0}", DateTime.Now));
+            var Dialog = new System.Windows.Forms.OpenFileDialog();
+            Dialog.Multiselect = true;
+            Dialog.Filter = "All files (*.*)|*.*";
+            if (args == null || args.Length == 0)
+            {
+                args = new string[] { string.Empty};
+            }
+
+            string directory = args[0];
+            if (string.IsNullOrWhiteSpace(directory))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(string.Format("Path is empty. Debugger will browse to your desktop."));
+                Console.ForegroundColor = ConsoleDefaultColor;
+                directory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+
+            if (!System.IO.Directory.Exists(directory))
+            {
+                Console.WriteLine($"{directory} is not a valid path.");
+                Console.WriteLine(string.Format("Debugging ended at: {0}", DateTime.Now));
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("");
+            Dialog.InitialDirectory = directory;
+            Dialog.ShowDialog();
+            Console.WriteLine("");
+            Console.WriteLine("----------------------------------------------"); 
+            Console.WriteLine("Press any key to end...");
+            Console.ReadLine();
+            Console.WriteLine(string.Format("Debugging ended at: {0}", DateTime.Now));
+            Console.ReadKey();
         }
+
+        
     }
+    
 }
--- PDMAddInDebugger/Properties/AssemblyInfo.cs
+++ PDMAddInDebugger/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PDMAddInDebugger")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tool to debug SOLIDWORKS PDM add-ins")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CADSharp LLC")]
 [assembly: AssemblyProduct("PDMAddInDebugger")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]

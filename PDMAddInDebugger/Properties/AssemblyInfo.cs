5016384677b3e34e8c96c8a62b2bf9ddef077252
jliliamen <amen@bluebyte.biz>
2020-11-12 13:38:40 -0800

    Incremented version

--- PDMAddInDebugger/Properties/AssemblyInfo.cs
+++ PDMAddInDebugger/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PDMAddInDebugger")]
 [assembly: AssemblyDescription("Tool to debug SOLIDWORKS PDM add-ins")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CADSharp LLC")]
+[assembly: AssemblyCompany("Blue Byte LLC")]
 [assembly: AssemblyProduct("PDMAddInDebugger")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
